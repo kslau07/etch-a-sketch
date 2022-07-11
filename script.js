b242af0127ed5aa1389333e17de5b03fdf5e685e
sam-laptop <cwm9889@gmail.com>
2022-07-11 00:40:18 -0400

    Add color change to "cells"

--- script.js
+++ script.js
@@ -2,7 +2,27 @@
 
 const container = document.querySelector('.container');
 
-const box = document.createElement('div');
-box.classList.add('content');
-box.textContent = "Hello world!"
-container.appendChild(box)
+for (i = 0; i < 32 ; i++) {
+
+  let div = document.createElement('div');
+  div.setAttribute('id', `box${i}`);
+  div.setAttribute('class', 'cell');
+  container.appendChild(div);
+  
+}
+
+
+
+function changeColor(event) {
+  const key = event.target.id
+  document.querySelector(`#${key}`).classList.add('changeColor')
+}
+
+function removeColor(event) {
+  const key = event.target.id;
+  document.querySelector(`#${key}`).classList.remove('changeColor')
+}
+
+const list = document.querySelectorAll('.cell')
+list.forEach(el => el.addEventListener('mouseover', changeColor))
+list.forEach(el => el.addEventListener('mouseout', removeColor))
